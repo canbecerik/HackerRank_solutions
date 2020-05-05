ceaf1f0f69d3e5cb3d045966a47cafc1b4b0d80b
Can Becerik <canbecerik@gmail.com>
2020-05-05 15:52:11 +0300

    Update progress

--- Tutorials/10_Days_of_Javascript/day7_2_js10-regexp-2-English.js
+++ Tutorials/10_Days_of_Javascript/day7_2_js10-regexp-2-English.js
@@ -28,7 +28,7 @@ function regexVar() {
      * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
      * followed by one or more letters.
      */
-    const re = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/;
+    const re = /^(?:Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/;
     
     /*
      * Do not remove the return statement
